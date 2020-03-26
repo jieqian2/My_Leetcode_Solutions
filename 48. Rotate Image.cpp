3532dbbdd7d8f5c4561c18626f0e71c4b9193184
jieqian2 <54562527+jieqian2@users.noreply.github.com>
2020-03-25 22:57:05 -0500

    Create 48. Rotate Image.cpp

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int n=matrix.size();
+        
+        for(int i=0; i<n; i++)
+            for(int j=i+1; j<n; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        
+        for(int j=0; j<n; j++)
+            for(int i=0; i<(n/2); i++)
+                swap(matrix[j][i], matrix[j][n-1-i]);   
+        
+    }
+};
